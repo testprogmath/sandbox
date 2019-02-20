a97195b3add3145040592e852e6826b4649ff45e
aa.khvorostyanova <aa.khvorostyanova@jet.msk.su>
2019-02-20 14:06:09 +0300

    Робот (недоделка)

---
+++ src/main/java/ru/stepic/base/robot/Robot.java
@@ -0,0 +1,148 @@
+package ru.stepic.base.robot;
+
+public class Robot {
+    int x = 0;
+    int y = 0;
+    Direction direction = Direction.UP;
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void turnLeft() {
+        System.out.println("Поворот против часовой стрелки");
+        if (direction == Direction.DOWN) {
+            this.direction = Direction.RIGHT;
+            return;
+        }
+
+        if (direction == Direction.UP) {
+            this.direction = Direction.LEFT;
+            return;
+        }
+
+        if (direction == Direction.LEFT) {
+            this.direction = Direction.DOWN;
+            return;
+        }
+
+        if (direction == Direction.RIGHT) {
+            this.direction = Direction.UP;
+            return;
+        }
+    }
+
+    public void turnRight() {
+        System.out.println("поворот по часовой стрелке");
+        if (this.direction == Direction.DOWN) {
+            System.out.println("Вниз -> влево");
+            this.direction = Direction.LEFT;
+            return;
+        }
+
+        if (this.direction == Direction.UP) {
+            System.out.println("Вверх -> вправо");
+            this.direction = Direction.RIGHT;
+            return;
+        }
+
+        if (this.direction == Direction.LEFT) {
+            System.out.println("Влево -> вверх");
+            this.direction = Direction.UP;
+            return;
+        }
+
+        if (this.direction == Direction.RIGHT) {
+            System.out.println("Вправо -> вниз");
+            this.direction = Direction.DOWN;
+            return;
+        }
+    }
+
+    public void stepForward() {
+        System.out.println("движение");
+        if (direction == Direction.DOWN) {
+            System.out.println("вниз");
+            this.y--;
+        }
+
+        if (direction == Direction.UP) {
+            System.out.println("вверх");
+            this.y++;
+        }
+
+        if (direction == Direction.LEFT) {
+            System.out.println("налево");
+            this.x--;
+        }
+
+        if (direction == Direction.RIGHT) {
+            System.out.println("направо");
+            this.x++;
+        }
+    }
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+            int x = robot.getX();
+            int y = robot.getY();//придумать, что сделать с private -доступом
+        if (toY>=0) {
+            while (robot.getY()<toY) {
+                robot.stepForward();
+                robot.y++;
+            }
+        }
+        else {
+            robot.turnLeft();
+            robot.turnLeft();
+            while (robot.getY()>toY){
+                robot.stepForward();
+                robot.y--;
+            }
+        }
+        Direction d = robot.getDirection();
+        if (toX>=0){
+            if (d==Direction.DOWN) {
+                robot.turnLeft();
+            }
+            else {
+                robot.turnRight();
+            }
+                while (robot.getX()<toX){
+                    robot.stepForward();
+                    robot.x++;
+                }
+            }
+        else {
+            if (d==Direction.DOWN) {
+                robot.turnRight();
+            }
+            else {
+                robot.turnLeft();
+            }
+            while (robot.getX()>toX){
+                robot.stepForward();
+                robot.x--;
+            }
+        }
+        }
+
+
+            }
+
+
+
